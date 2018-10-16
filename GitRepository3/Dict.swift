93109b8815e98b98cdebd106cae6cf6813d89eb1
Ian Vilhena <ian.vilhena@iteris.com.br>
2018-10-16 14:57:21 -0300

    Classe adicionada

---
+++ GitRepository3/Dict.swift
@@ -0,0 +1,13 @@
+//
+//  Dict.swift
+//  GitRepository3
+//
+//  Created by Ian Vilhena on 16/10/18.
+//  Copyright © 2018 Ian Vilhena. All rights reserved.
+//
+
+import UIKit
+
+class Dict: NSObject {
+
+}
